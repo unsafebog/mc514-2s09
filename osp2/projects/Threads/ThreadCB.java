18426622248d3258597072dde1262cc3896f1b3b
Lucas.Katayama <Lucas.Katayama@cec42c5e-a552-11de-bea2-1db044c32224>
2009-10-18 15:07:31 +0000

    [FUNCIONANDO] No Errors

--- osp2/projects/Threads/ThreadCB.java
+++ osp2/projects/Threads/ThreadCB.java
@@ -18,33 +18,27 @@ import osp.Resources.*;
 */
 public class ThreadCB extends IflThreadCB 
 {
-	//Create ready queue list
-	static GenericList readyQueue;
 
+	static GenericList readyQueue;
 	
 	public ThreadCB()
 	{
 		super();
+
+	}
+
+	public static void init()
+	{
 		readyQueue = new GenericList();
 	}
 
-	/**
-	This method will be called once at the beginning of the
-	simulation. The student can set up static variables here.
-
-	@OSPProject Threads
-	*/
-	public static void init()
-	{
-	}
-
-	/** 
-	Sets up a new thread and adds it to the given task. 
-	The method must set the ready status 
-	and attempt to add thread to task. If the latter fails 
-	because there are already too many threads in this task, 
-	so does this method, otherwise, the thread is appended 
-	to the ready queue and dispatch() is called.
+    /** 
+        Sets up a new thread and adds it to the given task. 
+        The method must set the ready status 
+        and attempt to add thread to task. If the latter fails 
+        because there are already too many threads in this task, 
+        so does this method, otherwise, the thread is appended 
+        to the ready queue and dispatch() is called.
 
 	The priority of the thread can be set using the getPriority/setPriority
 	methods. However, OSP itself doesn't care what the actual value of
@@ -53,252 +47,240 @@ public class ThreadCB extends IflThreadCB
 
 	@return thread or null
 
-	@OSPProject Threads
-	*/
-	public static ThreadCB do_create(TaskCB task)
+        @OSPProject Threads
+    */
+	static public ThreadCB do_create(TaskCB task)
 	{
-			//if task thread limit is not exceeded
-		if(task.getThreadCount() >= MaxThreadsPerTask)
+		//Test if thread creation is permitted
+		if(task.getThreadCount() < MaxThreadsPerTask)
+		{
+			
+				//Create Thread object
+			ThreadCB thread = new ThreadCB();
+			
+				//Link thread to a task
+			if(task.addThread(thread) == FAILURE)
+				return null;
+			thread.setTask(task);
+				//set priority --------------------
+			
+				//set new status
+			thread.setStatus(ThreadReady);
+				//put at readyqueue end
+			readyQueue.append(thread);
+			
+				//dispatch a new thread
+			ThreadCB.dispatch();
+			
+			return thread;
+		}
+		else
 		{
 			ThreadCB.dispatch();
 			return null;
 		}
-			//new thread object
-		ThreadCB newThread = new ThreadCB();
-			//add thread to task
-		if(task.addThread(newThread) == FAILURE)
-			return null;
-			//set thread task
-		newThread.setTask(task);
-		
-		//Set Priority --------------------------------------
-			//set new status
-		newThread.setStatus(ThreadReady);
-			//put in queue
-		readyQueue.append(newThread);
-		
-		ThreadCB.dispatch();
-		
-		return newThread;
-		
 	}
 
-	/** 
+    /** 
 	Kills the specified thread. 
 
 	The status must be set to ThreadKill, the thread must be
 	removed from the task's list of threads and its pending IORBs
 	must be purged from all device queues.
-
+        
 	If some thread was on the ready queue, it must removed, if the 
 	thread was running, the processor becomes idle, and dispatch() 
 	must be called to resume a waiting thread.
-
+	
 	@OSPProject Threads
-	*/                                                                                   
+    */
 	public void do_kill()
 	{
-		Device device;
 		TaskCB task = this.getTask();
-		
 		switch(this.getStatus())
 		{
-				//if ready remove from ready queue
-			case ThreadReady:
-				readyQueue.remove(this); 
+			case ThreadKill:
 				break;
-				//if running remove cpu control then remove
-			case ThreadRunning: 
-				ThreadCB.dispatch();
+			case ThreadReady:
 				readyQueue.remove(this);
 				break;
-				//if ThreadWaiting+n cancel pending IO
-			default:
-				for(int i = 0; i < Device.getTableSize(); i++)
+			case ThreadRunning:
+				MMU.getPTBR().getTask().setCurrentThread(null);
+				MMU.setPTBR(null);
+				break;
+			default://ThreadWaiting at any level
+				for(int i=0; i < Device.getTableSize(); i++)
 					Device.get(i).cancelPendingIO(this);
 				break;
-		} 
-			//give up thread resources
-		ResourceCB.giveupResources(this);
-			//Set status
-		this.setStatus(ThreadKill);
-			//Remove the dead thread
+		}
+				this.setStatus(ThreadKill);
 		task.removeThread(this);
-			//if has no more threads, kill task
+		ResourceCB.giveupResources(this);
 		if(task.getThreadCount() == 0)
+		{
 			task.kill();
-			//Dipatch new thread
+		}
 		ThreadCB.dispatch();
+		
 	}
 
-	/** Suspends the thread that is currenly on the processor on the 
-	specified event. 
+    /** Suspends the thread that is currenly on the processor on the 
+        specified event. 
 
-	Note that the thread being suspended doesn't need to be
-	running. It can also be waiting for completion of a pagefault
-	and be suspended on the IORB that is bringing the page in.
-
+        Note that the thread being suspended doesn't need to be
+        running. It can also be waiting for completion of a pagefault
+        and be suspended on the IORB that is bringing the page in.
+	
 	Thread's status must be changed to ThreadWaiting or higher,
-	the processor set to idle, the thread must be in the right
-	waiting queue, and dispatch() must be called to give CPU
-	control to some other thread.
+        the processor set to idle, the thread must be in the right
+        waiting queue, and dispatch() must be called to give CPU
+        control to some other thread.
 
 	@param event - event on which to suspend this thread.
 
-	@OSPProject Threads
-	*/
+        @OSPProject Threads
+    */
 	public void do_suspend(Event event)
 	{
 		switch(this.getStatus())
 		{
+			case ThreadKill:
+			case ThreadReady:
+				return;
 			case ThreadRunning:
-				this.setStatus(ThreadWaiting);
+				event.addThread(MMU.getPTBR().getTask().getCurrentThread());
+				MMU.getPTBR().getTask().getCurrentThread().setStatus(ThreadWaiting);
 				MMU.getPTBR().getTask().setCurrentThread(null);
 				MMU.setPTBR(null);
-				event.addThread(this);
 				break;
-			case ThreadReady:
-				readyQueue.remove(this);
-				this.setStatus(ThreadWaiting);
-				event.addThread(this);
-				return;
-			case ThreadWaiting:
-				event.removeThread(this);
-				this.setStatus(this.getStatus()+1);
-				event.addThread(this);
-				break;
-			default:
+			default://Thread already waiting
 				event.removeThread(this);
 				this.setStatus(this.getStatus()+1);
 				event.addThread(this);
 				break;
 		}
-		//dispatch new thread
 		ThreadCB.dispatch();
-		
 	}
 
-	/** Resumes the thread.
-
+    /** Resumes the thread.
+        
 	Only a thread with the status ThreadWaiting or higher
 	can be resumed.  The status must be set to ThreadReady or
 	decremented, respectively.
 	A ready thread should be placed on the ready queue.
-
+	
 	@OSPProject Threads
-	*/
+    */
 	public void do_resume()
 	{
-			//Analog to Figure 2.1 from OSP book
 		switch(this.getStatus())
 		{
-			case ThreadRunning:
+			case ThreadKill:
 			case ThreadReady:
 				return;
 			case ThreadWaiting:
 				this.setStatus(ThreadReady);
+				readyQueue.append(this);
 				break;
-				//ThreadWaiting+n case
 			default:
 				this.setStatus(this.getStatus()-1);
 				break;
 		}
-			//Append thread ready on queue
-		if(this.getStatus() == ThreadReady)
-			readyQueue.append(this);
 		ThreadCB.dispatch();
 	}
 
-	/** 
-	Selects a thread from the run queue and dispatches it. 
+    /** 
+        Selects a thread from the run queue and dispatches it. 
 
-	If there is just one theread ready to run, reschedule the thread 
-	currently on the processor.
+        If there is just one theread ready to run, reschedule the thread 
+        currently on the processor.
 
-	In addition to setting the correct thread status it must
-	update the PTBR.
-
+        In addition to setting the correct thread status it must
+        update the PTBR.
+	
 	@return SUCCESS or FAILURE
 
-	@OSPProject Threads
-	*/
+        @OSPProject Threads
+    */
 	public static int do_dispatch()
 	{
-		ThreadCB runningThread, readyThread;
-		
-			//If no running thread
-		if(MMU.getPTBR() == null)
+			//get the first in queue
+		ThreadCB thread = (ThreadCB) readyQueue.removeHead();
+			
+			//Context Switching
+		if(MMU.getPTBR() != null && thread != null)
+		{//thread running, preempt
+			readyQueue.append(MMU.getPTBR().getTask().getCurrentThread());
+			MMU.getPTBR().getTask().getCurrentThread().setStatus(ThreadReady);
+			MMU.getPTBR().getTask().setCurrentThread(null);
+			MMU.setPTBR(null);
+			//dispatch
+			thread.setStatus(ThreadRunning);
+			MMU.setPTBR(thread.getTask().getPageTable());
+			thread.getTask().setCurrentThread(thread);
+			
+		}
+		else if(MMU.getPTBR() != null && thread == null)
+		{//current thread continues on cpu
+			return SUCCESS;
+		}
+		else if(MMU.getPTBR() == null && thread != null)
+		{//put thread on cpu
+			thread.setStatus(ThreadRunning);
+			MMU.setPTBR(thread.getTask().getPageTable());
+			thread.getTask().setCurrentThread(thread);
+		}
+		else if(MMU.getPTBR() == null && thread == null)
 		{
-			if(readyQueue.isEmpty())
-			{
-				return FAILURE;
-			}
-			readyThread = (ThreadCB) readyQueue.removeHead();
-			
-			if(readyThread == null)
-			{
-				return FAILURE;
-			}
-			readyThread.setStatus(ThreadRunning);
-			MMU.setPTBR(readyThread.getTask().getPageTable());
-			readyThread.getTask().setCurrentThread(readyThread);
-			return SUCCESS;
-		}	
-			//Get current running thread
-		runningThread = MMU.getPTBR().getTask().getCurrentThread();	
-			//if current running thread didn't spend quantum time, keep current running thread
-		if(runningThread.getTimeOnCPU()%2 < 1)
-			return SUCCESS;
-		
-			//else, preempt running thread
-		runningThread.setStatus(ThreadReady);
-		readyQueue.append(runningThread);
-		MMU.setPTBR(null);
-		runningThread.getTask().setCurrentThread(null);
-			//dispatching
-			
-		readyThread = (ThreadCB) readyQueue.removeHead();
-			//if thre is no ready to run thread, put last running thread to run
-		while(readyThread == null)
-			readyThread = (ThreadCB) readyQueue.removeHead();
-		readyThread.setStatus(ThreadRunning);
-		MMU.setPTBR(readyThread.getTask().getPageTable());
-		readyThread.getTask().setCurrentThread(readyThread);
-
-		return SUCCESS;
+			return FAILURE;
+		}
+		return FAILURE;
 	}
 
-	/**
-	Called by OSP after printing an error message. The student can
-	insert code here to print various tables and data structures in
-	their state just after the error happened.  The body can be
-	left empty, if this feature is not used.
+    /**
+       Called by OSP after printing an error message. The student can
+       insert code here to print various tables and data structures in
+       their state just after the error happened.  The body can be
+       left empty, if this feature is not used.
 
-	@OSPProject Threads
-	*/
-	public static void atError()
+       @OSPProject Threads
+    */
+    public static void atError()
+    {
+        // your code goes here
+
+    }
+
+    /** Called by OSP after printing a warning message. The student
+        can insert code here to print various tables and data
+        structures in their state just after the warning happened.
+        The body can be left empty, if this feature is not used.
+       
+        @OSPProject Threads
+     */
+    public static void atWarning()
+    {
+        // your code goes here
+
+    }
+
+	public static void removeKilled()
 	{
-		System.out.printf("Error Here\n\n\n");
-
+		Enumeration ready = readyQueue.forwardIterator();
+		ThreadCB thread;
+		while(ready.hasMoreElements())
+		{
+			thread = (ThreadCB) ready.nextElement();
+			if(thread.getStatus() == ThreadKill)
+				readyQueue.remove(thread);
+		}
 	}
+    /*
+       Feel free to add methods/fields to improve the readability of your code
+    */
 
-	/** Called by OSP after printing a warning message. The student
-	can insert code here to print various tables and data
-	structures in their state just after the warning happened.
-	The body can be left empty, if this feature is not used.
-
-	@OSPProject Threads
-	*/
-	public static void atWarning()
-	{
-	// your code goes here
-
-	}
-
-	
-	
-	
-	
-	
 }
 
+/*
+      Feel free to add local classes to improve the readability of your code
+*/
