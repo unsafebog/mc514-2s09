e0924048489a0128f70e2676bef6f3857bc13462
Lucas.Katayama <Lucas.Katayama@cec42c5e-a552-11de-bea2-1db044c32224>
2009-10-23 18:48:18 +0000

    [antes]

--- osp2/projects/Resources/RRB.java
+++ osp2/projects/Resources/RRB.java
@@ -1,10 +1,12 @@
 package osp.Resources;
 
-import java.util.*;
 
-import osp.Utilities.*;
+import java.util.*;
 import osp.IFLModules.*;
+import osp.Tasks.*;
 import osp.Threads.*;
+import osp.Utilities.*;
+import osp.Memory.*;
 
 /**
    The studends module for dealing with resource management. The methods 
@@ -14,40 +16,25 @@ import osp.Threads.*;
 */
 
 public class RRB extends IflRRB
-{
-    /** 
-        constructor of class RRB 
-        Creates a new RRB object. This constructor must have
-        super() as its first statement.
+{   
+	ResourceCB resource;
+	ThreadCB thread;
+	int status;
+	int ID;
+	int quantity;
+	
+	public RRB(ThreadCB th, ResourceCB res , int quant)
+	{
+		super();
+		quantity = quant;
+		resource = res;
+		thread = th;
+	}
 
-        @OSPProject Resources
-    */   
-    public RRB(ThreadCB thread, ResourceCB resource,int quantity)
-    {
-        // your code goes here
-
-    }
-
-    /**
-       This method is called when we decide to grant an RRB.
-       The method must update the various resource quantities
-       and notify the thread waiting on the granted RRB.
-
-        @OSPProject Resources
-    */
-    public void do_grant()
-    {
-        // your code goes here
-
-    }
-
-
-    /*
-       Feel free to add methods/fields to improve the readability of your code
-    */
-
+	public void do_grant()
+	{
+		int qty = this.resource.getAllocated(this.thread);
+		this.resource.setAvailable(this.resource.getAvailable()-this.quantity);
+		this.resource.setAllocated(this.thread, this.quantity+qty);
+	}
 }
-
-/*
-      Feel free to add local classes to improve the readability of your code
-*/
