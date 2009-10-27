18c5202c3d28bdfafa8e12b87ef2e0b9b22285b9
Lucas.Katayama <Lucas.Katayama@cec42c5e-a552-11de-bea2-1db044c32224>
2009-10-27 21:02:22 +0000

    [codigo proprio] codigo do outro naum funciona e nao intendi

--- trunk/osp2/projects/Resources/RRB.java
+++ trunk/osp2/projects/Resources/RRB.java
@@ -1,40 +1,49 @@
 package osp.Resources;
 
-
 import java.util.*;
-import osp.IFLModules.*;
-import osp.Tasks.*;
-import osp.Threads.*;
+
 import osp.Utilities.*;
-import osp.Memory.*;
-
-/**
-   The studends module for dealing with resource management. The methods 
-   that have to be implemented are do_grant().
-
-   @OSPProject Resources
-*/
+import osp.IFLModules.*;
+import osp.Threads.*;
 
 public class RRB extends IflRRB
-{   
-	ResourceCB resource;
+{
+	int id;
 	ThreadCB thread;
-	int status;
-	int ID;
+	ResourceCB resource;
 	int quantity;
+	int status;
 	
-	public RRB(ThreadCB th, ResourceCB res , int quant)
+
+	public RRB(ThreadCB th, ResourceCB res ,int qty)
 	{
-		super();
-		quantity = quant;
-		resource = res;
+		super(th, res, qty);
+		id = this.getID();
 		thread = th;
+		resource = res;
+		quantity = qty;
+		status = this.getStatus();
 	}
 
 	public void do_grant()
 	{
-		int qty = this.resource.getAllocated(this.thread);
-		this.resource.setAvailable(this.resource.getAvailable()-this.quantity);
-		this.resource.setAllocated(this.thread, this.quantity+qty);
+		this.resource.setAvailable(this.resource.getAvailable() - this.quantity);
+		this.resource.setAllocated(this.thread,
+			this.resource.getAllocated(this.thread) + this.quantity);
+			
+		this.setStatus(Granted);
+		this.status = this.getStatus();
+		
+		this.notifyThreads();
 	}
+
+
+    /*
+       Feel free to add methods/fields to improve the readability of your code
+    */
+
 }
+
+/*
+      Feel free to add local classes to improve the readability of your code
+*/
