8284cbb8e50302d23a385eede995e5ad7dbe9845
lucas.katayama <lucas.katayama@cec42c5e-a552-11de-bea2-1db044c32224>
2009-11-19 19:48:28 +0000

    -FrameTableEntry.java done!

--- trunk/osp2/projects/Memory/FrameTableEntry.java
+++ trunk/osp2/projects/Memory/FrameTableEntry.java
@@ -13,28 +13,8 @@ import osp.IFLModules.IflFrameTableEntry;
 
 public class FrameTableEntry extends IflFrameTableEntry
 {
-    /**
-       The frame constructor. Must have
-
-       	   super(frameID)
-	   
-       as its first statement.
-
-       @OSPProject Memory
-    */
-    public FrameTableEntry(int frameID)
-    {
-        // your code goes here
-
-    }
-
-
-    /*
-       Feel free to add methods/fields to improve the readability of your code
-    */
-
+	public FrameTableEntry(int frameID)
+	{
+		super(frameID);
+	}
 }
-
-/*
-      Feel free to add local classes to improve the readability of your code
-*/
